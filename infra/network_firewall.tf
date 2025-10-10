ff79f1e71df820d0ee8a45a0ffa8edc7fd653859
Kyle Lisk <klisk@venacorp.com>
2025-10-10 12:47:37 -0600

    Fixed terraform fmt issue

--- infra/network_firewall.tf
+++ infra/network_firewall.tf
@@ -83,7 +83,7 @@ resource "aws_networkfirewall_firewall" "main" {
   name                = "${var.project_name}-firewall"
   firewall_policy_arn = aws_networkfirewall_firewall_policy.main.arn
   vpc_id              = aws_vpc.main.id
-  delete_protection = true
+  delete_protection   = true
 
   # Deploy firewall endpoints in dedicated firewall subnets
   dynamic "subnet_mapping" {
